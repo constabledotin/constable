4fd9bb7ce46235cf14e88b6db1f01202b3a20b9b
engineers_bro <kartikchauhan336@gmail.com>
2024-07-19 22:29:21 +0530

    fized error

--- src/app/admin/(routes)/dashboard/overview/page.jsx
+++ src/app/admin/(routes)/dashboard/overview/page.jsx
@@ -273,10 +273,11 @@ function OverView() {
                                 {
                                     users.map((user) => {
                                         return (
-                                            <tr className="border-b dark:border-gray-700">
+                                            <tr key={user._id} className="border-b dark:border-gray-700">
                                                 <th
                                                     scope="row"
                                                     className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                                
                                                 >
                                                     {user.name}
                                                 </th>
