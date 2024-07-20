e145e2ddd41e98dd310b21349291202183378723
engineers_bro <kartikchauhan336@gmail.com>
2024-07-20 13:22:01 +0530

    added key

--- src/app/admin/(routes)/dashboard/user/all/page.jsx
+++ src/app/admin/(routes)/dashboard/user/all/page.jsx
@@ -278,7 +278,7 @@ function AllUser() {
                             <tbody>
                                 {users.map((user) => {
                                     return (
-                                        <tr className="border-b dark:border-gray-700">
+                                        <tr className="border-b dark:border-gray-700 " key={user._id}>
                                             <th
                                                 scope="row"
                                                 className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
